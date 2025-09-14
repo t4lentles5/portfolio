ad5c027dbedff7937f2b36e20b13f29f781c18c6
Omar Obregon <oobregon544@gmail.com>
2025-09-14 17:42:03 -0500

    Refactor Footer and Header components; add contactInfo data structure and update index page imports

---
+++ src/lib/data.ts
@@ -0,0 +1,17 @@
+export const contactInfo = [
+  {
+    name: 'Email',
+    url: 'mailto:oobregon544@gmail.com',
+    icon: 'icon-[mdi--email]',
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/t4lentles5',
+    icon: 'icon-[mdi--github]',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/adolfo-omar-obregón-henostroza-b0a5b129b',
+    icon: 'icon-[mdi--linkedin]',
+  },
+];
