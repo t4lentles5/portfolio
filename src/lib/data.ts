5c310bf521ea9cd3f37f4079c916c7fbadc4a650
Omar Obregon <oobregon544@gmail.com>
2025-09-23 16:04:43 -0500

    feat: enhance skills section with new data and styling

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -15,3 +15,50 @@ export const contactInfo = [
     icon: 'icon-[mdi--linkedin]',
   },
 ];
+
+export const skills = {
+  programmingLanguages: [
+    {
+      name: 'JavaScript',
+      icon: 'icon-[logos--javascript]',
+    },
+    {
+      name: 'TypeScript',
+      icon: 'icon-[logos--typescript-icon]',
+    },
+
+    {
+      name: 'Python',
+      icon: 'icon-[logos--python]',
+    },
+  ],
+  frontendDevelopment: [
+    { name: 'HTML', icon: 'icon-[logos--html-5]' },
+    { name: 'CSS', icon: 'icon-[logos--css-3]' },
+    {
+      name: 'React',
+      icon: 'icon-[logos--react]',
+    },
+    {
+      name: 'Next.js',
+      icon: 'icon-[logos--nextjs-icon]',
+    },
+    {
+      name: 'Astro',
+      icon: 'icon-[logos--astro-icon]',
+    },
+    {
+      name: 'Tailwind CSS',
+      icon: 'icon-[logos--tailwindcss-icon]',
+    },
+  ],
+  backendDevelopment: [
+    { name: 'Node.js', icon: 'icon-[logos--nodejs-icon]' },
+    { name: 'Express', icon: 'icon-[lineicons--expressjs]' },
+    { name: 'Nest.js', icon: 'icon-[logos--nestjs]' },
+  ],
+  databases: [
+    { name: 'PostgreSQL', icon: 'icon-[logos--postgresql]' },
+    { name: 'MongoDB', icon: 'icon-[logos--mongodb-icon]' },
+  ],
+};
